ff8db26f7316415c85d693c6ba83d08657c7369e
Rohit Khavanekar <rohit.oyc9@gmail.com>
2020-05-11 12:08:18 +0530

    Initial commit

---
+++ src/test/day2.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 
+{
+	@Test
+	public void homeloan()
+	{
+		System.out.println("HomeLoan");
+	}
+	
+	@Test
+	public void CarLoan()
+	{
+		System.out.println("Carloan");
+	}
+	
+	@Test
+	public void PersonaLoan()
+	{
+		System.out.println("PersonalLoan");
+	}
+	
+
+}
+
