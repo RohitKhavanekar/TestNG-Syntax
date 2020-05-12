7fbbc803912b37ff44618f758a16895615a8d21d
Rohit Khavanekar <rohit.oyc9@gmail.com>
2020-05-12 14:16:59 +0530

    2-12-2020

--- src/test/day1.java
+++ src/test/day1.java
@@ -1,10 +1,12 @@
 package test;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day1 
 {
-	@Test
+	@Test(groups= {"Smoke"})
 	public void demo()
 	{
 		System.out.println("Hello");
@@ -14,6 +16,18 @@ public class day1
 	public void demo2()
 	{
 		System.out.println("Bye");
+	}
+	
+	@BeforeTest
+	public void demo3()
+	{
+		System.out.println("Exeute First");
+	}
+	
+	@BeforeMethod
+	public void demo4()
+	{
+		System.out.println("Running Before Every Method");
 	}
 
 }
--- src/test/day2.java
+++ src/test/day2.java
@@ -1,10 +1,13 @@
 package test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day2 
 {
-	@Test
+	@Test(groups= {"Smoke"})
 	public void homeloan()
 	{
 		System.out.println("HomeLoan");
@@ -16,10 +19,27 @@ public class day2
 		System.out.println("Carloan");
 	}
 	
-	@Test
+	@Test(dependsOnMethods = ("CarLoan"))
 	public void PersonaLoan()
 	{
 		System.out.println("PersonalLoan");
+	}
+	@BeforeTest
+	public void Runner()
+	{
+		System.out.println("Execute Last");
+	}
+	
+	@BeforeMethod
+	public void Bfmeth()
+	{
+		System.out.println("Running Before Methods");
+	}
+	
+	@AfterMethod
+	public void Afmeth()
+	{
+		System.out.println("Running after method");
 	}
 	
 
